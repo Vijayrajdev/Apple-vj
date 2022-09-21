c8f3b1af37d4497bbbfb80b357e5f8b77bee2d0f
Vijayrajdev <vijayrajck@outlook.com>
2022-09-21 21:52:00 +0530

    Basket fully updated

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -5,11 +5,14 @@ import { useSelector } from "react-redux";
 import Button from "../components/Button";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Header from "../components/Header";
-import { selectBasketItems } from "../Redux/basketSlice";
+import { selectBasketItems, selectBasketTotal } from "../Redux/basketSlice";
+import Currency from "react-currency-formatter";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
 const checkout = () => {
   const items = useSelector(selectBasketItems);
   const router = useRouter();
+  const basketTotal = useSelector(selectBasketTotal);
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
     {} as { [key: string]: Product[] }
   );
@@ -50,6 +53,73 @@ const checkout = () => {
             {Object.entries(groupedItemsInBasket).map(([key, items]) => (
               <CheckoutProduct key={key} items={items} id={key} />
             ))}
+
+            <div className="my-12 mt-6 ml-auto max-w-3xl">
+              <div className="divide-y divide-gray-300">
+                <div className="pb-4">
+                  <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p>
+                      <Currency quantity={basketTotal} currency="INR" />
+                    </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Shipping</p>
+                    <p>FREE</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex flex-col gap-x-1 lg:flex-row">
+                      Estimated tax for:{" "}
+                      <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+                        Enter zip code
+                        <ChevronDownIcon className="h-6 w-6" />
+                      </p>
+                    </div>
+                    <p>₹ -</p>
+                  </div>
+                </div>
+
+                <div className="flex justify-between pt-4 text-xl font-semibold">
+                  <h4>Total</h4>
+                  <h4>
+                    <Currency quantity={basketTotal} currency="INR" />
+                  </h4>
+                </div>
+              </div>
+
+              <div className="my-14 space-y-4">
+                <h4 className="text-xl font-semibold">
+                  How would you like to check out?
+                </h4>
+                <div className="flex flex-col gap-4 md:flex-row">
+                  <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      <span>Pay Monthly</span>
+                      <span>with Apple Card</span>
+                      <span>
+                        ₹22,631.39/mo. at 0% APR<sup className="-top-1">◊</sup>
+                      </span>
+                    </h4>
+                    <Button title="Check Out with Apple Card Monthly Installments" />
+                    <p className="mt-2 max-w-[240px] text-[13px]">
+                      0.00 due today, which includes applicable full-price
+                      items, down payments, shipping, and taxes.
+                    </p>
+                  </div>
+
+                  <div className="flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 md:order-2">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      Pay in full
+                      <span>
+                        <Currency quantity={basketTotal} currency="INR" />
+                      </span>
+                    </h4>
+
+                    <Button noIcon title="Check Out" width="w-full" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </main>
